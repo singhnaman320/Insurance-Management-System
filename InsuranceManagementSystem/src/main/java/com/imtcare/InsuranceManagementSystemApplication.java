2a383932900cf3ec60376cde69088e214c2a2c8c
naman kumar singh <singhnaman320@gmail.com>
2023-04-03 11:20:26 +0530

    SpringBoot basic setup

---
+++ InsuranceManagementSystem/src/main/java/com/imtcare/InsuranceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imtcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagementSystemApplication.class, args);
+	}
+
+}
