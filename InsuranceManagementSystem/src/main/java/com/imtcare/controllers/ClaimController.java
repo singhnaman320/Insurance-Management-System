3c7b39938396392bdd82b4a925ef491590f3da75
naman kumar singh <singhnaman320@gmail.com>
2023-04-08 00:48:51 +0530

    Modification in Controller class for REST API request

--- InsuranceManagementSystem/src/main/java/com/imtcare/controllers/ClaimController.java
+++ InsuranceManagementSystem/src/main/java/com/imtcare/controllers/ClaimController.java
@@ -1,5 +1,86 @@
 package com.imtcare.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imtcare.entities.Claim;
+import com.imtcare.exceptions.ClaimNotFoundException;
+import com.imtcare.services.ClaimServices;
+
+@RestController
+@RequestMapping("/api/claims")
 public class ClaimController {
 
+	@Autowired
+	private ClaimServices claimServices;
+	
+	/*
+	
+	// Registering claim on policy
+	
+	@PostMapping("/")
+    public ResponseEntity<Claim> createNewClaimHandler(@RequestBody Claim claim) throws ClaimNotFoundException{
+    	
+        Claim addClaim = claimServices.createNewClaim(claim);
+        
+        return new ResponseEntity<Claim>(addClaim, HttpStatus.CREATED);
+    }
+
+	
+	// Fetching all the claims
+	
+    @GetMapping("/")
+    public ResponseEntity<List<Claim>> fetchAllClaimsHandler() throws ClaimNotFoundException{
+    	
+        List<Claim> allclaims = claimServices.fetchAllClaims();
+        
+        return new ResponseEntity<List<Claim>>(allclaims, HttpStatus.OK);
+    }
+
+    
+    // Fetching claim details by claim Id
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Claim> fetchClaimByIdHandler(@PathVariable("id") Integer claimId) throws ClaimNotFoundException{
+    	
+        Claim claimById = claimServices.fetchClaimById(claimId);
+        
+        return new ResponseEntity<Claim>(claimById, HttpStatus.OK);
+    }
+
+    
+    // Updating claim details by claim Id
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Claim> updateClaimByIdHandler(@PathVariable("id") Integer claimId, @RequestBody Claim claim) 
+    																						throws ClaimNotFoundException{
+    	
+        Claim updateClaim = claimServices.updateClaimById(claimId, claim);
+        
+        return new ResponseEntity<Claim>(updateClaim, HttpStatus.ACCEPTED);
+    }
+
+    
+    // Deleting claim by claim Id
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Claim> deleteClaimByIDHandler(@PathVariable("id") Integer claimId) throws ClaimNotFoundException{
+    	
+        Claim deleteClaim = claimServices.deleteClaimById(claimId);
+        
+        return new ResponseEntity<>(deleteClaim, HttpStatus.OK);
+    }
+    
+    */
 }
--- InsuranceManagementSystem/src/main/java/com/imtcare/controllers/InsurancePolicyController.java
+++ InsuranceManagementSystem/src/main/java/com/imtcare/controllers/InsurancePolicyController.java
@@ -1,5 +1,87 @@
 package com.imtcare.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imtcare.entities.InsurancePolicy;
+import com.imtcare.exceptions.PolicyNotFoundException;
+import com.imtcare.services.InsurancePolicyServices;
+
+
+@RestController
+@RequestMapping("/api/policies")
 public class InsurancePolicyController {
 
+	@Autowired
+	private InsurancePolicyServices insurancePolicyServices;
+	
+	
+	// Registering new policy
+	
+	@PostMapping("/")
+	public ResponseEntity<InsurancePolicy> createNewInsurancePolicyHandler(@RequestBody InsurancePolicy insurancePolicy) 
+																						throws PolicyNotFoundException{
+		
+		InsurancePolicy addPolicy = insurancePolicyServices.createNewInsurancePolicy(insurancePolicy);
+		
+		return new ResponseEntity<InsurancePolicy>(addPolicy, HttpStatus.CREATED);
+	}
+	
+	
+	// Getting all the policy details
+	
+	@GetMapping("/")
+    public ResponseEntity<List<InsurancePolicy>> fetchAllInsurancepoliciesHandler() throws PolicyNotFoundException{
+		
+        List<InsurancePolicy> allpolicyDetails = insurancePolicyServices.fetchAllInsurancepolicies();
+        
+        return new ResponseEntity<List<InsurancePolicy>>(allpolicyDetails, HttpStatus.OK);
+    }
+
+	
+	// getting policy details by Id
+	
+    @GetMapping("/{id}")
+    public ResponseEntity<InsurancePolicy> fetchInsurancePolicyByIdHandler(@PathVariable("id") Integer policyId) 
+    																				throws PolicyNotFoundException{
+    	
+        InsurancePolicy getpolicyById = insurancePolicyServices.fetchInsurancePolicyById(policyId);
+        
+        return new ResponseEntity<InsurancePolicy>(getpolicyById, HttpStatus.OK);
+    }
+
+    
+    // Updating policy details by policy Id
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<InsurancePolicy> updateInsurancePolicyByIdHandler(@PathVariable("id") Integer policyId, InsurancePolicy insurancePolicy)
+    																													throws PolicyNotFoundException{
+    	
+        InsurancePolicy updatepolicy = insurancePolicyServices.updateInsurancePolicyById(policyId, insurancePolicy);
+        
+        return new ResponseEntity<InsurancePolicy>(updatepolicy, HttpStatus.ACCEPTED);
+    }
+
+    
+    // Delete policy by policy Id
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<InsurancePolicy> deletePolicyByIDHandler(@PathVariable("id") Integer policyId) 
+    																		throws PolicyNotFoundException{
+    	
+        InsurancePolicy deletePolicy = insurancePolicyServices.deleteInsurancePolicyById(policyId);
+        
+        return new ResponseEntity<InsurancePolicy>(deletePolicy, HttpStatus.OK);
+    }
 }
